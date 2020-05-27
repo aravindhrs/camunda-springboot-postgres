f3dec2dfdae13b8f453614ee2c5ad9fb4897e411
ARAVINDH RS <aravindhrs@outlook.com>
2020-05-28 02:01:27 +0530

    Camunda Spring Boot Project - Postgres and Authorization enabled

---
+++ src/main/java/com/example/workflow/config/CamundaSecurityFilter.java
@@ -0,0 +1,29 @@
+package com.example.workflow.config;
+
+import javax.servlet.Filter;
+import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamundaSecurityFilter {
+
+  @Bean
+  public FilterRegistrationBean<Filter> processEngineAuthenticationFilter() {
+    FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+    registration.setName("camunda-auth");
+    registration.setFilter(getProcessEngineAuthenticationFilter());
+    registration.addInitParameter("authentication-provider",
+        "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+    registration.addUrlPatterns("/*");
+    registration.setOrder(1);
+    return registration;
+  }
+
+  @Bean
+  public Filter getProcessEngineAuthenticationFilter() {
+    return new ProcessEngineAuthenticationFilter();
+  }
+}
+
